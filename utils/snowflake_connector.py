93b40bdbf2e19afd6718c7ccac9a3e0cc0e3754c
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2026-01-05 14:53:39 +0530

    Update snowflake_connector.py

--- utils/snowflake_connector.py
+++ utils/snowflake_connector.py
@@ -1,25 +1,20 @@
 import snowflake.connector
-import pandas as pd
 
-
-def get_snowflake_connection(creds: dict):
-    """
-    Create and return a Snowflake connection
-    """
+def get_snowflake_connection(
+    account,
+    user,
+    password,
+    warehouse,
+    database,
+    schema,
+    role
+):
     return snowflake.connector.connect(
-        account=creds["account"],
-        user=creds["user"],
-        password=creds["password"],
-        warehouse=creds["warehouse"],
-        database=creds["database"],
-        schema=creds["schema"],
-        role=creds["role"],
+        account=account,
+        user=user,
+        password=password,
+        warehouse=warehouse,
+        database=database,
+        schema=schema,
+        role=role
     )
-
-
-def fetch_table_df(conn, database, schema, table):
-    """
-    Load full Snowflake table into Pandas DataFrame
-    """
-    query = f'SELECT * FROM "{database}"."{schema}"."{table}"'
-    return pd.read_sql(query, conn)
